36ef48a638df6262b3d3bc1bdf7e990f09f53cca
Anmol Gautam <tarptaeya@gmail.com>
2019-11-17 14:43:49 +0530

    published to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='Truck',
-    version='0.1',
+    name='trucklang',
+    version='0.1.0',
     author='Anmol Gautam',
+    author_email="tarptaeya@gmail.com",
+    description="A dynamic object oriented programming language with a focus on simplicity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/tarptaeya/truck",
     packages=['truck'],
-    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
 )
